8742b9eafb74ff2ecf3cd93331469aaa80058862
Night0wIX <night0wix.01@gmail.com>
2026-08-17 20:29:22 +0300

    fix(client): fix login and signup schemas

--- client/src/features/auth/schemas/sign-up.schema.ts
+++ client/src/features/auth/schemas/sign-up.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const signUpSchema = z
   .object({
-    email: z.string().min(1, "Email is required").email("Enter a valid email"),
+    email: z.email("Enter a valid email"),
     password: z.string().min(8, "Password must be at least 8 characters"),
     confirmPassword: z.string().min(1, "Confirm your password"),
   })
